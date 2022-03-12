41af555b8ce71c1ac0bfb00ce4a9ee57fe1b2c6d
Weslley Vander <90974762+weslleyg2@users.noreply.github.com>
2022-03-12 13:46:07 -0300

    update of javascript error

--- js/script.js
+++ js/script.js
@@ -2,25 +2,44 @@
     'use strict';
 
     const $singUp = document.getElementById('singUp');
-    const $radios = document.querySelectorAll('.checkboxRadial');
+    const $labels = document.querySelectorAll('label');
+    const $label = document.querySelector('.checkboxRadial');
+    const $email = document.querySelector('.email');
+    const $form = document.querySelector('.form');
 
-    $singUp.addEventListener('clicked', authenticateClick);
-
-    function authenticateClick() {
-        if ($singUp.value === '') {
-            alert('Preencha os campos vazios:')
-            $singUp.style.cssText =
+    $form.addEventListener('submit', authenticateClick);
+    function authenticateClick(e) {
+        if (!$email.value) {
+            $email.style.cssText =
                 'border: 2px solid magenta;';
+            alert('Preencha os campos vazios:');
+            e.preventDefault();
 
         }
-        return $singUp;
-        let $radio = document.querySelectorAll('.checkboxRadial');
+            
+            if (!$labels[0].checked && !$labels[1].checked) {
+                console.log($labels[i]);
+                $label.style.cssText = 'color: magenta;' +
+                    'border: 2px solid magenta;';
+                e.preventDefault();
+            }
+               
+            
+           
+            /* else if(!$labels[i-1].checked && $labels[i].checked){
+                $labels[i].style.cssText = 'color: magenta;' +
+                    'border: 2px solid magenta;';
+                e.preventDefault();
+            } */
+            
+        }
+
+        /* let $radio = document.querySelectorAll('#label');
         for (var i = 0; i < radios.length; i++) {
             if (radios[i].checked) {
                 console.log("Escolheu: " + radios[i].value);
             }
-        }
-
-    }
-
-}) ();
+        } */
+        return $singUp;
+    
+})();
